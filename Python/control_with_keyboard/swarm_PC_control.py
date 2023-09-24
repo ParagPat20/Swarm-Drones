04bf777eabed5e75e1f80d38b04297ecaf5a262a
Parag Patil <parag.pat20@gmail.com>
2023-09-24 21:29:33 +0530

    Hello

--- Python/control_with_keyboard/swarm_PC_control.py
+++ Python/control_with_keyboard/swarm_PC_control.py
@@ -2,16 +2,19 @@ import socket
 import threading
 import tkinter as tk
 from tkinter import ttk
+import json
 import keyboard
 from ttkthemes import ThemedStyle
 
-# Initialize P and C dictionaries for one drone
+# Initialize P and C dictionaries for two drones
 P = {
-    0: {'Batt': 0, 'Groundspeed': 0, 'ARM': 0, 'GPS': 0, 'Altitude': 0, 'MODE': None, 'VelocityX': 0, 'VelocityY': 0, 'VelocityZ': 0}
+    0: {'Batt': 0, 'Groundspeed': 0, 'ARM': 0, 'GPS': 0, 'Altitude': 0, 'MODE': None, 'VelocityX': 0, 'VelocityY': 0, 'VelocityZ': 0},
+    1: {'Batt': 0, 'Groundspeed': 0, 'ARM': 0, 'GPS': 0, 'Altitude': 0, 'MODE': None, 'VelocityX': 0, 'VelocityY': 0, 'VelocityZ': 0}
 }
 
 C = {
     0: {'vx': 0, 'vy': 0, 'vz': 0, 'Arming': 0, 'Mode': 'GUIDED', 'Takeoff': 0},
+    1: {'vx': 0, 'vy': 0, 'vz': 0, 'Arming': 0, 'Mode': 'GUIDED', 'Takeoff': 0},
     'drone': None
 }
 
@@ -29,43 +32,71 @@ def set_control(drone_id):
     C['drone'] = drone_id
 
 # Create frames to hold labels
-frame_labels = [ttk.Frame(root, relief="solid", padding=10) for _ in range(2)]
+frame_labels = [[ttk.Frame(root, relief="solid", padding=10) for _ in range(3)] for _ in range(2)]
 for i in range(2):
-    frame_labels[i].grid(row=i, column=0)
+    for j in range(3):
+        frame_labels[i][j].grid(row=i, column=j)
 
 # Create a dictionary to store labels for P dictionary
-p_labels = {0: {}}
+p_labels = {0: {}, 1: {}}
 for i, (key, value) in enumerate(P[0].items()):
-    p_label = ttk.Label(frame_labels[0], text=f'D1 P: {key}:     ')
+    p_label = ttk.Label(frame_labels[0][0], text=f'D1 P: {key}:     ')
     p_label.config(font=("Helvetica", 14))  # Big Font
     p_label.grid(row=i, column=0, sticky="w")
     
-    p_value = ttk.Label(frame_labels[0], text=str(value), justify="left")
+    p_value = ttk.Label(frame_labels[0][0], text=str(value), justify="left")
     p_value.config(font=("Helvetica", 14))  # Big Font
     p_value.grid(row=i, column=1, sticky="w")
     
     p_labels[0][key] = p_value
 
-# Create a dictionary to store labels for C dictionary
-c_labels = {0: {}}
-for i, (key, value) in enumerate(C[0].items()):
-    c_label = ttk.Label(frame_labels[0], text=f'D1 C: {key}:     ')
-    c_label.config(font=("Helvetica", 14))  # Big Font
-    c_label.grid(row=i, column=2, sticky="w")
+for i, (key, value) in enumerate(P[1].items()):
+    p_label = ttk.Label(frame_labels[1][0], text=f'D2 P: {key}:     ')
+    p_label.config(font=("Helvetica", 14))  # Big Font
+    p_label.grid(row=i, column=0, sticky="w")
     
-    c_value = ttk.Label(frame_labels[0], text=str(value), justify="left")
+    p_value = ttk.Label(frame_labels[1][0], text=str(value), justify="left")
+    p_value.config(font=("Helvetica", 14))  # Big Font
+    p_value.grid(row=i, column=1, sticky="w")
+    
+    p_labels[1][key] = p_value
+
+# Create a dictionary to store labels for C dictionary
+c_labels = {0: {}, 1: {}}
+for i, (key, value) in enumerate(C[0].items()):
+    c_label = ttk.Label(frame_labels[0][2], text=f'D1 C: {key}:     ')
+    c_label.config(font=("Helvetica", 14))  # Big Font
+    c_label.grid(row=i, column=0, sticky="w")
+    
+    c_value = ttk.Label(frame_labels[0][2], text=str(value), justify="left")
     c_value.config(font=("Helvetica", 14))  # Big Font
-    c_value.grid(row=i, column=3, sticky="w")
+    c_value.grid(row=i, column=1, sticky="w")
     
     c_labels[0][key] = c_value
 
-# Create battery progress bar and label for the drone
-battery_label = ttk.Label(frame_labels[0], text="D1 Battery Level:")
-battery_label.config(font=("Helvetica", 14))  # Big Font
-battery_label.grid(row=len(P[0]), column=0, sticky="w")
+for i, (key, value) in enumerate(C[1].items()):
+    c_label = ttk.Label(frame_labels[1][2], text=f'D2 C: {key}:     ')
+    c_label.config(font=("Helvetica", 14))  # Big Font
+    c_label.grid(row=i, column=0, sticky="w")
+    
+    c_value = ttk.Label(frame_labels[1][2], text=str(value), justify="left")
+    c_value.config(font=("Helvetica", 14))  # Big Font
+    c_value.grid(row=i, column=1, sticky="w")
+    
+    c_labels[1][key] = c_value
 
-battery_bar = ttk.Progressbar(frame_labels[0], length=100, mode="determinate", style="Custom.Horizontal.TProgressbar")
-battery_bar.grid(row=len(P[0]), column=1, sticky="w", padx=10)
+# Create battery progress bars and labels for both drones
+battery_labels = [ttk.Label(frame_labels[0][1], text="D1 Battery Level:"), ttk.Label(frame_labels[1][1], text="D2 Battery Level:")]
+battery_labels[0].config(font=("Helvetica", 14))  # Big Font
+battery_labels[1].config(font=("Helvetica", 14))  # Big Font
+battery_labels[0].grid(row=0, column=0, sticky="w")
+battery_labels[1].grid(row=0, column=0, sticky="w")
+
+battery_bars = [ttk.Progressbar(frame_labels[0][1], length=100, mode="determinate", style="Custom.Horizontal.TProgressbar"),
+               ttk.Progressbar(frame_labels[1][1], length=100, mode="determinate", style="Custom.Horizontal.TProgressbar")]
+
+for i in range(2):
+    battery_bars[i].grid(row=0, column=1, sticky="w", padx=10)
 
 # Define custom styles for progress bar
 style.configure("Custom.Horizontal.TProgressbar", troughcolor="white")
@@ -80,13 +111,18 @@ def map_battery_level(battery_level):
 
 # Create buttons for control selection at the bottom
 control_button1 = ttk.Button(root, text="Drone 1", command=lambda: set_control(0))
+control_button2 = ttk.Button(root, text="Drone 2", command=lambda: set_control(1))
+control_button3 = ttk.Button(root, text="Both", command=lambda: set_control(-1))
+
 
 control_button1.grid(row=2, column=0, padx=10, pady=10, sticky="w")
+control_button2.grid(row=2, column=1, padx=10, pady=10, sticky="w")
+control_button3.grid(row=2, column=2, padx=10, pady=10, sticky="w")
 
 # Create a label to display the currently controlled drone
 controlled_drone_label = ttk.Label(root, text="Controlled Drone: None")
 controlled_drone_label.config(font=("Helvetica", 14))
-controlled_drone_label.grid(row=3, column=0, padx=10, pady=10, sticky="w")
+controlled_drone_label.grid(row=3, column=0, columnspan=3, padx=10, pady=10, sticky="w")
 
 def update_controlled_drone_label():
     drone_id = control_var.get()
@@ -94,6 +130,8 @@ def update_controlled_drone_label():
         controlled_drone_label.config(text="Controlled Drone: Both")
     elif drone_id == 0:
         controlled_drone_label.config(text="Controlled Drone: Drone 1")
+    elif drone_id == 1:
+        controlled_drone_label.config(text="Controlled Drone: Drone 2")
     else:
         controlled_drone_label.config(text="Controlled Drone: None")
     root.after(100, update_controlled_drone_label)
@@ -113,16 +151,17 @@ def update_gui(drone_id):
     # Update the battery bar
     battery_level = P[drone_id]['Batt']
     battery_percentage = map_battery_level(battery_level)
-    battery_bar["value"] = battery_percentage
+    battery_bars[drone_id]["value"] = battery_percentage
 
     if battery_level <= 10.8:
-        battery_bar["style"] = "Red.Horizontal.TProgressbar"
+        battery_bars[drone_id]["style"] = "Red.Horizontal.TProgressbar"
     else:
-        battery_bar["style"] = "Green.Horizontal.TProgressbar"
+        battery_bars[drone_id]["style"] = "Green.Horizontal.TProgressbar"
 
     root.after(100, update_gui, drone_id)
 
 update_gui(0)
+update_gui(1)
 
 def handle_client(drone_id, client_socket):
     global P, C
@@ -132,14 +171,30 @@ def handle_client(drone_id, client_socket):
             p_str = client_socket.recv(1024).decode()
             P[drone_id] = eval(p_str)  # Convert the received string back to a dictionary
 
-            # Process the received data as needed for the selected drone
-            update_gui(drone_id)
+            # Receive P dictionary values for the other drone(s)
+            if drone_id == -1:  # Control both drones
+                for other_drone_id in range(2):
+                    if other_drone_id != drone_id:
+                        p_str = client_socket.recv(1024).decode()
+                        P[other_drone_id] = eval(p_str)
+            else:
+                other_drone_id = 1 if drone_id == 0 else 0  # Toggle between drones
+                p_str = client_socket.recv(1024).decode()
+                P[other_drone_id] = eval(p_str)
 
-            # Send C dictionary values for the selected drone
+            # Process the received data as needed for the selected drone(s)
+            if drone_id == -1:  # Control both drones
+                for i in range(2):
+                    update_gui(i)
+            else:
+                update_gui(drone_id)
+
+            # Send C dictionary values for the selected drone(s)
             c_str = str(C[drone_id])
             c_drone = str(C['drone'])
             client_socket.send(c_str.encode())
             client_socket.send(c_drone.encode())
+            controller(drone_id)
 
         except KeyboardInterrupt:
             print(f"Server for drone {drone_id} stopped.")
@@ -150,6 +205,8 @@ def handle_client(drone_id, client_socket):
         if keyboard.is_pressed('esc'):
             print(f"Client for drone {drone_id} closed.")
             break
+
+
 
 def controller(drone_id):
     MAX_VELOCITY = 1
@@ -232,12 +289,16 @@ def PC_SERVER_start(drone_id):
             print(f"Connection to drone {drone_id} closed.")
             break
 
+        # Toggle to the next drone ID (0 or 1)
+        drone_id = 1 if drone_id == 0 else 0
+
 
 if __name__ == "__main__":
-    # Start the server for the drone in a separate thread
-    server_thread = threading.Thread(target=PC_SERVER_start, args=(0,))
-    server_thread.daemon = True
-    server_thread.start()
+    for drone_id in range(2):
+        # Start the server for each drone in a separate thread
+        server_thread = threading.Thread(target=PC_SERVER_start, args=(drone_id,))
+        server_thread.daemon = True
+        server_thread.start()
 
     # Start the Tkinter GUI main loop
     root.mainloop()
