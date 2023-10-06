1b9adf68d1219cc3f10659023da2c9899ce7ee22
Parag Patil <parag.pat20@gmail.com>
2023-10-06 20:38:18 +0530

    Hello

--- Python/control_with_keyboard/test3.py
+++ Python/control_with_keyboard/test3.py
@@ -1,16 +1,9 @@
-from dronekit import connect, VehicleMode
-import time
+from dronekit import connect
+
+# Connect to the drone
 d1 = connect('tcp:127.0.0.1:5762')
-while True:
-    prev_hearbeat = d1.last_heartbeat
-    print('prev',prev_hearbeat)
-    time.sleep(5)
-    print('last',d1.last_heartbeat)
-    if d1.last_heartbeat == prev_hearbeat:
-        d1.close()
-        print("Reconnecting to drone")
-        d1 = connect('tcp:127.0.0.1:5762')
-        print("Connected to drone")
-    else:
-        print("Drone is up to date")
-        time.sleep(1)
+
+
+# Close the vehicle object before exiting
+print("Close vehicle object")
+d1.close()
